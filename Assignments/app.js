30c8c3b47fc568d2d9539d9d238cd9767aee2d6d
Hunain-baig <xunainali4@gmail.com>
2024-02-21 17:28:08 +0500

    add folders

---
+++ Assignments/app.js
@@ -0,0 +1,191 @@
+// CHAPTER ONE:
+// Q1:
+
+// alert();
+
+// Q2:
+
+// alert('Error! Please enter a valid password');
+
+// Q3:
+
+// alert('Welcome to JS land.. \n Happy coding!');
+
+// Q4:
+
+// alert('Welcome to JS land');
+
+// alert('Happy coding! \n Prevent this page from additional dialogs');
+
+// Q5: Ans is ---> Simple you create popup by usning console when you inspect the page, simply type alert('') and type message between the brackets 
+
+// CHAPTER TWO:
+// Q1:
+
+// var username = '';
+
+// Q2:
+
+// var username = 'Honey Baig';
+
+// Q3:
+
+// var message = 'Hello world';
+
+// alert(message);
+
+
+// Q4:
+
+// var firstName = 'Jhon Doe';
+// alert(firstName)
+
+// var age = 15 + " years old";
+// alert(age)
+
+// var message = 'Certified Mobile Application Development';
+// alert(message)
+
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+// Q5:
+
+// alert('Pizza \n Pizz \n Piz \n Pi \n P');
+
+// Q6:
+
+// var email = 'My email address is xunainali4@gmail.com';
+// alert(email)
+
+// Q7:
+
+// var book = 'i am tyring to learn from the book. A smarter \n way to learn javascript';
+// alert(book)
+
+// Q8:
+
+// document.body.innerHTML = 'Yah! i can write content through javascript';
+
+// Q9:
+
+// var charac = '';
+// alert(charac)
+
+// CHAPTER THREE:
+// Q1:
+
+// var age = 21;
+// alert('Your age is ' + age);
+
+// Q2:
+
+
+
+// Q3:
+
+// var birthyear = 2001;
+// document.body.innerHTML = 'My birth year is 2001 <br /> data type of my declared variable is number';
+
+
+
+// CHAPTER FOUR:
+//Q1:
+
+// var a, b, c
+
+// Q2:
+// LEGAL VARIABLE
+
+// var myVariable
+// var _myVariable
+// var $myVariable
+// var myVariable12
+// var my_variable
+// var va12riable
+
+// IILEGAL VARIABLE
+
+// var 12myVariable
+// var my Variable 
+// var my-Variable
+// var !myVariable
+// var @myVariable
+
+// Q3: Rules for variable
+// Ans: Varible can only start with letters not number
+//      No special characters use in the stating of variable except two characters i.e "$" and "_"
+//      Javascript is case sensitive programming language if you dont follow the syntax rules accuratley you will get Error!
+
+// CHAPTER FIVE:
+// Q1:
+
+// Q2:
+
+// Q3:
+
+// Q4:
+
+// Q5:
+
+// Q6:
+
+// Q7:
+
+// Q8:
+
+// Q9:
+
+// Q10:
+
+// Q11:
+
+// Q12:
+
+// Q13:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lecture-01/app.js
@@ -0,0 +1,81 @@
+// console.log("Hellow World");
+
+// alert("Hellow World");
+
+// var firstName = "hunain";
+// console.log (typeof(firstName));
+// var firstName = 12;
+// console.log (typeof(firstName));
+// var firstName = true;
+// console.log (typeof(firstName));
+
+// var username = prompt ("enter your first name");
+// console.log ("hellow" + username);
+// console.log (typeof(username));
+
+// var username = prompt ("enter your last name")
+// console.log ("lastname" + username);
+// console.log (typeof(username));
+
+// var username = prompt ("enter your age")
+// console.log ("yourage" + username);
+// console.log (typeof(username));
+
+// console.log ('Heloo hunain');
+
+// alert ('Hye!')
+
+// var firstName = 'hunain';
+// console.log (typeof(firstName));
+
+// var firstName = 12;
+// console.log (typeof(firstName));
+
+// var firstName = false;
+// console.log (typeof(firstName));
+
+// var firstName = prompt ('Enter your firstname');
+// var lastName = prompt ('Enter your lastname');
+// var age = prompt ('enter your age');
+
+// console.log('my name is ' + firstName + " "  +  lastName + 'my age is' + age);
+
+// var firstName = prompt('enter your firstname');
+// var lastName = prompt('enter your lastname');
+// var age = prompt('enter your age');
+
+// console.log('my name is ' + firstName + " " + lastName + " and my age is " + age )
+
+// var firstName = prompt ('enter your name');
+// var lastName = prompt ('enter you lastname');
+// var age = prompt ('enter your age');
+
+// console.log ('my name is ' + firstName + " " + lastName + ' my age is ' + age);
+
+// var firstName = prompt ('name');
+// var lastName = prompt ('lastname');
+// var age = prompt ('age');
+
+// console.log ('my name is ' + firstName + " " + lastName + ' my age is' + age);
+
+// var username = prompt ('Enter Your Username');
+// console.log ( " hello " + username );
+// console.log(typeof(username));
+
+// var password = prompt ('Enter your password');
+// console.log ( "hello" + password );
+// console.log(typeof(password));
+
+// var email = prompt ('Enter your email');
+// console.log ("hello" + email);
+// console.log (typeof(email));
+
+
+// var username = prompt ('Enter your username');
+// var lastName = prompt ('Enter your lastname');
+// var email = prompt ('Enter your email');
+// var age = prompt ('enter your age');
+// var password = prompt ('Enter your password');
+
+// console.log ( ' my Name is ' + username  + " " + lastName + ' my email is ' + email  + ' my age is ' + " " + age + 'my password is ' + " " + password);
+
